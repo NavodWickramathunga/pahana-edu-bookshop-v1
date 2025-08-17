ca71e2eb19e2001efa43182238a62c5c6bac48e1
Navod Wickramathunga <49792272+NavodWickramathunga@users.noreply.github.com>
2025-08-17 12:56:05 +0530

    Initialize Spring Boot billing system project
    Set up a new Spring Boot project for the Pahana Edu Billing System with MongoDB integration. Added Maven wrapper, project structure, basic application entry point, initial configuration, and placeholder static HTML files.

---
+++ billing-system/src/main/java/com/pahanaedu/billingsystem/PahanaEduBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pahanaedu.billingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaEduBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahanaEduBillingSystemApplication.class, args);
+	}
+
+}
